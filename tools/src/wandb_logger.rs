30510c02231670f9decbe3812f5bc852d577c509
p4lik4ri <asakel64@gmail.com>
2026-05-04 16:39:19 +0300

    updated to send metrics to wandb

--- tools/src/wandb_logger.rs
+++ tools/src/wandb_logger.rs
@@ -98,7 +98,7 @@ impl WandbLogger {
             .header("Authorization", format!("Bearer {api_key}"))
             .json(&json!({
                 "query": "mutation UpsertBucket($name: String, $project: String, $entity: String) \
-                          { upsertBucket(input: {name: $name, projectName: $project, entityName: $entity}) \
+                          { upsertBucket(input: {name: $name, project: $project, entity: $entity}) \
                             { bucket { id name } } }",
                 "variables": {
                     "name": run_name,
